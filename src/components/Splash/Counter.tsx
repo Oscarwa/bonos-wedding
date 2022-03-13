598f51064de2f492d468f15f9fe0b6ba7a679aee
Oscar Barba <oscar.barba@smagus.io>
2022-03-13 14:14:12 -0600

    - Added goal images

--- src/components/Splash/Counter.tsx
+++ src/components/Splash/Counter.tsx
@@ -7,20 +7,27 @@ import Milestones from "./Milestones";
 import goal1 from "../../images/goal1.png";
 import goal2 from "../../images/goal2.png";
 import goal3 from "../../images/goal3.png";
+import goal4 from "../../images/goal4.png";
+import goal5 from "../../images/goal5.png";
+import goal6 from "../../images/goal6.webp";
+import goal7 from "../../images/goal7.png";
+import goal8 from "../../images/goal8.png";
+import goal9 from "../../images/fav.png";
+import goal10 from "../../images/goal10.png";
+import goal11 from "../../images/goal11.webp";
 
-const images = [goal1, goal2, goal3];
+const images = [goal1, goal2, goal3, goal4, goal5, goal6, goal7, goal8, goal9, goal10, goal11];
 
 const Counter: FC = () => {
   const app = useFirebaseApp();
   const db = getDatabase(app);
   const countRef = ref(db, "counter");
 
+  const { status, data: counter } = useDatabaseObjectData<number>(countRef);
   const getImage = () => {
-    const hour = new Date().getHours();
-    return images[hour % images.length];
+    return images[counter % images.length];
   };
 
-  const { status, data: counter } = useDatabaseObjectData<number>(countRef);
 
   const handleHeartClick = () => {
     const newVal = counter + 1;
@@ -36,7 +43,7 @@ const Counter: FC = () => {
       <Milestones progress={counter} />
       <Container className="text-center">
         <Button variant="link" onClick={handleHeartClick}>
-          <img width={200} src={getImage()} alt="counter icon" />
+          <img height={200} src={getImage()} alt="counter icon" className="grayscale p-3"/>
         </Button>
         <div className="letters fs-3">
           clicked {counter.toLocaleString()} times
